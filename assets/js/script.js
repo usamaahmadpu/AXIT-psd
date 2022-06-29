3e77f4b5555a79d5b60883e5abf81678edbce75d
usamaahmadpu <usamaahmadpucit@gmail.com>
2022-06-29 10:59:46 +0500

    extra files

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+
+  //Toggle Nav bar
+
+  $(document).ready(function(){
+    $(".fa-bars").click(function(){
+      $(".main-menu").fadeToggle();
+    });
+  });
+
+   // slick
+
+   $(".testimonial-row").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 912,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 540,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    ]
+    });
+
+    // Sticky nav bar
+
+    window.addEventListener('scroll', function(){
+      let navbar = document.getElementById("header-menu");
+
+          if(window.pageYOffset >= 90){
+              navbar.classList.add('sticky');
+          }else{
+              navbar.classList.remove('sticky');
+          }
+  });
+
+
+ 
